cd0379338558ac774d54f33e9ab2f4024f457733
Adithya Hegde <adithyah07@gmail.com>
2022-08-15 01:41:52 +0530

    initial iteration

---
+++ pg-csv.py
@@ -0,0 +1,53 @@
+import argparse
+import getpass
+import configparser
+import psycopg2
+import uuid
+
+
+
+parser = argparse.ArgumentParser(description='Export data from PostgreSQL to CSV')
+# Fetch from config file
+parser.add_argument('-c', '--config', help='path to config file,\
+                    no need to specify any other arguments if this is specified')
+#Fetch from command line
+parser.add_argument('-n', '--hostname', help='hostname')
+parser.add_argument('-p', '--port', help='port')
+parser.add_argument('-d', '--dbname', help='database name')
+parser.add_argument('-u', '--user', help='username')
+parser.add_argument('-q', '--query', help='query')
+args = parser.parse_args()
+
+
+# Resolve DB Connection String
+if args.config:
+    config = configparser.ConfigParser()
+    config.read(args.config)
+    host, dbname, port, user, password = config['conn_string'].values()
+else:
+    host = args.hostname if args.hostname else 'localhost'
+    dbname = args.dbname if args.dbname else 'postgres'
+    port = args.port if args.port else '5432'
+    user = args.user if args.user else input('Username: ')
+    password = getpass.getpass('Password: ')
+
+
+# Connect to DB
+db_conn = psycopg2.connect(host=host, dbname=dbname,\
+    port=port, user=user, password=password)
+db_cursor = db_conn.cursor()
+
+
+# Fetch Queries
+if args.query:
+    queries = args.query.split(';')
+else:
+    queries = config['queries'].values()
+
+
+# Execute Queries and Write to CSV
+output_dir = config['output']['dir']
+for query in queries:
+    filename = output_dir + str(uuid.uuid4()) + '.csv'
+    csv_query = "COPY ({}) TO {} WITH CSV HEADER".format(query,filename)
+    db_cursor.execute(csv_query)
